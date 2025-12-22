dd6bffd79cca8af530e68af67bb1f4ea0047519a
helingcai <hzz0214@outlook.com>
2025-12-22 21:16:55 +0800

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -125,7 +125,7 @@ def context(browser, request):
         allure.attach.file(
             trace,
             name="Playwright-Trace.zip",
-            attachment_type=allure.attachment_type.ZIP # Allure会自动识别.zip后缀文件
+            attachment_type=allure.attachment_type.BINARY # Allure会自动识别.zip后缀文件
         )
         attach_open_trace_command(trace)
 
@@ -258,6 +258,7 @@ cd {project_root} && npx playwright show-trace {rel_posix}
         name="Open Playwright Trace",
         attachment_type=allure.attachment_type.TEXT
     )
+
 
 
 
